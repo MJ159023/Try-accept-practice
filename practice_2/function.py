114df087b91e653395181c192ba2ea576e8fafe0
MJ159023 <michael.bruneau@stu.ocsb.ca>
2024-11-20 18:00:41 +0000

    saving code

---
+++ practice_2/function.py
@@ -0,0 +1,16 @@
+
+def ask_user_for_number():
+    """
+    Asks User for number and then checks to make sure it's number before returning.
+    Gives error message if User inputs anything but a number
+    """
+
+    while True:
+        try:
+            print("Hello please input a number.")
+            user_choice = int(input())
+            return user_choice
+        except ValueError:
+            print("Please input only numbaers. Try again")
+            print("")
+
---
+++ practice_2/main.py
@@ -0,0 +1,9 @@
+import function
+
+def main():
+    """Runs the program"""
+
+    print(function.ask_user_for_number())
+
+main()
+
